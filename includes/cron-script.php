544036b98ee85b89bf6de6a2ea884a260ded11f8
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-10-28 11:22:40 +0600

    (feat): Cron Script Created

--- includes/cron-script.php
+++ includes/cron-script.php
@@ -1,3 +1,33 @@
 <?php
 
+function CreateAllSimilarPosts()
+{
+
+    $args = array(
+        'post_type' => 'post',
+        'posts_per_page' => -1,
+        // 'order' => $sort_by,
+        // 'orderby' => 'title',
+        'post_status' => 'publish',
+        // 'tag' => $tags,
+        // 'ignore_sticky_posts' => 1,
+    );
+    
+    $post_list = get_posts($args);  
+
+    $posts = array();
+
+    foreach ( $post_list as $post ) {
+        foreach($post_list as $current)
+        {
+            if($post->ID != $current->ID)
+            { 
+                // Send the $current post to the API
+                // Get the score
+                // Write it to the database
+            }
+        }
+    }
+}
+
 ?>
