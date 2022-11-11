7f63e19c1583818be2c7f4ef189a6bf11c944486
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-11-11 14:08:56 +0600

    (fix): Api call fixed

--- includes/cron-script.php
+++ includes/cron-script.php
@@ -17,10 +17,10 @@ function CreateAllSimilarPosts()
         foreach ($post_list as $current) {
             if ($post->ID != $current->ID) {
                 $post_categories = get_the_category($post->ID);
-                $result = GetSimilarTextFromAPI($post->post_content, $current->post_content);
-                var_dump($result);
+                sleep(20);
+                $result = GetSimilarTextFromAPI(wp_strip_all_tags($post->post_content), wp_strip_all_tags($current->post_content));
                 foreach ($post_categories as $cat) {
-                    CreateSimilarPosts($post->ID, $current->ID, $result, $cat->term_id);
+                    CreateSimilarPosts($post->ID, $current->ID,$result, $cat->term_id);
                 }
             }
         }
