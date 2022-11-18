9c7eb0436feee80aea09ff3e64b25ad133391401
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-11-19 02:14:33 +0600

    (fix):Api endpoints fixed

--- admin/admin-menu.php
+++ admin/admin-menu.php
@@ -1,178 +1,177 @@
-<?         ob_start(); ?>
-<!Doctype html>
-<html>
+<?php
+require_once(ABSPATH . 'wp-content/plugins/recommendly/includes/database-operations.php');
 
-<head>
+/**
+ * Register a custom menu admin page
+ */
 
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+function register_my_custom_menu_page()
+{
 
-    <style>
-        .flex-container {
-            display: flex;
-            justify-content: space-between;
-        }
+    add_menu_page(
+        __('Recommendly Manager', 'textdomain'),
+        'Recommendly',
+        'manage_options',
+        'recommendly.php',
+        'show_table',
+        'dashicons-code-standards',
+        85
+    );
+}
+add_action('admin_menu', 'register_my_custom_menu_page');
 
-        #output-content {
-            height: 450px;
-            font-size: 12px;
-            width: 99%;
-            border: none;
-            border-top-style: none;
-            background-color: white;
-            color: #000;
-        }
+function show_table()
+{
+    ob_start();
+?>
 
-        .loader {
-            border: 16px solid #f3f3f3;
-            border-radius: 50%;
-            border-top: 16px solid blue;
-            border-bottom: 16px solid blue;
-            width: 120px;
-            height: 120px;
-            -webkit-animation: spin 2s linear infinite;
-            animation: spin 2s linear infinite;
-        }
+    <!Doctype html>
+    <html>
 
-        @-webkit-keyframes spin {
-            0% {
-                -webkit-transform: rotate(0deg);
+    <head>
+
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+
+        <style>
+            .flex-container {
+                display: flex;
+                justify-content: space-between;
             }
 
-            100% {
-                -webkit-transform: rotate(360deg);
+            #output-content {
+                height: 450px;
+                font-size: 12px;
+                width: 99%;
+                border: none;
+                border-top-style: none;
+                background-color: white;
+                color: #000;
             }
-        }
 
-        @keyframes spin {
-            0% {
-                transform: rotate(0deg);
+            .loader {
+                border: 16px solid #f3f3f3;
+                border-radius: 50%;
+                border-top: 16px solid blue;
+                border-bottom: 16px solid blue;
+                width: 120px;
+                height: 120px;
+                -webkit-animation: spin 2s linear infinite;
+                animation: spin 2s linear infinite;
             }
 
-            100% {
-                transform: rotate(360deg);
+            @-webkit-keyframes spin {
+                0% {
+                    -webkit-transform: rotate(0deg);
+                }
+
+                100% {
+                    -webkit-transform: rotate(360deg);
+                }
             }
-        }
-    </style>
 
-    <script>
-        async function postData(data, url) {
-            $("#loadingSpinner").show();
-            return fetch(url, {
-                    method: 'POST', // or 'PUT',
-                    mode: 'cors', // no-cors, *cors, same-origin
-                    headers: {
-                        'Content-Type': 'application/json',
+            @keyframes spin {
+                0% {
+                    transform: rotate(0deg);
+                }
 
-                    },
-                    body: JSON.stringify(data),
-                })
-                .then((response) => response.text())
-                .then(function(data) {
-                    $("#loadingSpinner").hide();
-                    alert("Task successfully Done");
-                })
-                .catch((error) => {
-                    console.error('Error:', error);
-                    alert("Warning ! Task failed !");
+                100% {
+                    transform: rotate(360deg);
+                }
+            }
+        </style>
+
+        <script>
+            var siteUrl = "<?php get_site_url(); ?>";
+            async function postData(data, url) {
+                $("#loadingSpinner").show();
+                return fetch(url, {
+                        method: 'POST', // or 'PUT',
+                        mode: 'cors', // no-cors, *cors, same-origin
+                        headers: {
+                            'Content-Type': 'application/json',
+
+                        },
+                        body: JSON.stringify(data),
+                    })
+                    .then((response) => response.text())
+                    .then(function(data) {
+                        $("#loadingSpinner").hide();
+                        alert("Task successfully Done");
+                    })
+                    .catch((error) => {
+                        console.error('Error:', error);
+                        alert("Warning ! Task failed !");
+                    });
+            }
+
+            $("document").ready(function() {
+                var cronOptions = <?php echo get_option('cron_links'); ?>;
+                console.log(cronOptions);
+                if (cronOptions == 1) {
+                    $("#enable").prop("checked", true);
+                } else {
+                    $("#disable").prop("checked", true);
+                }
+
+
+                $("#loadingSpinner").hide();
+                $("#apiSubmit").click(function() {
+                    var apiKey = $('#apiField').val();
+
+                    var data = {
+                        key: apiKey
+                    };
+
+                    postData(data, siteUrl + '/wp-json/recommendly/v1/saveapikey');
                 });
-        }
-
-        $("document").ready(function() {
-            var cronOptions =  <?php echo get_option('cron_links');?> 
-            console.log(cronOptions);
-            if(cronOptions == 1)
-            {
-                $("#enable").prop("checked" , true);  
-            } else {
-                $("#disable").prop("checked" , true);  
-            }
 
 
-            $("#loadingSpinner").hide();
-            $("#apiSubmit").click(function() {
-                var apiKey = $('#apiField').val();
+                $("#cilfap").click(function() {
+                    var king = {
+                        test: 'myTest'
+                    };
 
-                var data = {
-                    key: apiKey
-                };
+                    postData(king, siteUrl + '/wp-json/recommendly/v1/createinternallinksforallposts');
 
-                postData(data, 'http://127.0.0.1/wp-json/recommendly/v1/saveapikey');
+                });
+
+                $("#rilfap").click(function() {
+                    var wim = {
+                        lyk: 'myTest'
+                    };
+
+                    postData(wim, siteUrl + '/wp-json/recommendly/v1/removeinternallinks');
+                });
+
+                $("#cfnu").click(function() {
+                    var wer = {
+                        ker: 'myTest'
+                    };
+
+                    postData(wer, siteUrl + '/wp-json/recommendly/v1/updates');
+                });
+
+                $("#enable").click(function() {
+                    var optionenable = $(this).val();
+                    var eData = {
+                        option: optionenable
+                    };
+                    postData(eData, siteUrl + '/wp-json/recommendly/v1/cronoption');
+                });
+
+                $("#disable").click(function() {
+                    var optiondisable = $(this).val();
+                    var dData = {
+                        option: optiondisable
+                    };
+                    postData(dData, siteUrl + '/wp-json/recommendly/v1/cronoption');
+                });
+
             });
+        </script>
+    </head>
 
-
-            $("#cilfap").click(function() {
-                var king = {
-                    test: 'myTest'
-                };
-
-                postData(king, 'http://127.0.0.1/wp-json/recommendly/v1/createinternallinksforallposts');
-
-            });
-            
-            $("#rilfap").click(function() {
-                var wim = {
-                    lyk: 'myTest'
-                };
-
-                postData(wim, 'http://127.0.0.1/wp-json/recommendly/v1/removeinternallinks');
-            });
-
-            $("#cfnu").click(function() {
-                var wer = {
-                    ker: 'myTest'
-                };
-
-                postData(wer, 'http://127.0.0.1/wp-json/recommendly/v1/updates');
-            });
-
-            $("#enable").click(function() {
-                var optionenable = $(this).val();
-                var eData = {
-                    option: optionenable
-                };
-                postData(eData, 'http://127.0.0.1/wp-json/recommendly/v1/cronoption');
-            });
-
-            $("#disable").click(function() {
-                var optiondisable = $(this).val();
-                var dData = {
-                    option: optiondisable
-                };
-                postData(dData, 'http://127.0.0.1/wp-json/recommendly/v1/cronoption');
-            });
-
-        });
-    </script>
-</head>
-
-<body>
-
-    <?php
-    require_once(ABSPATH . 'wp-content/plugins/recommendly/includes/database-operations.php');
-
-    /**
-     * Register a custom menu admin page
-     */
-
-    function register_my_custom_menu_page()
-    {
-
-        add_menu_page(
-            __('Recommendly Manager', 'textdomain'),
-            'Recommendly',
-            'manage_options',
-            'recommendly.php',
-            'show_table',
-            'dashicons-code-standards',
-            85
-        );
-    }
-    add_action('admin_menu', 'register_my_custom_menu_page');
-
-    function show_table()
-    {
-
-    ?>
+    <body>
 
         <div class="flex-container">
             <div style="margin-right: 60px;">
@@ -245,15 +244,15 @@
 
         </div>
 
-    <?php
+    </body>
 
-        $content = ob_get_contents();
-        ob_end_clean();
-        echo $content;
-    }
+    </html>
 
-    ?>
+<?php
 
-</body>
+    $content = ob_get_contents();
+    ob_end_clean();
+    echo $content;
+}
 
-</html>
+?>
