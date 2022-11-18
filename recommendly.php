2d3f29079ea539e84845e8707337d135f3f4131f
faisalakandha <57830958+faisalakandha@users.noreply.github.com>
2022-11-19 01:33:18 +0600

    (fix): Plugin Activation Fixed

--- recommendly.php
+++ recommendly.php
@@ -1,18 +1,38 @@
 <?php
+
 /**
-* Plugin Name: Recommendly
-* Plugin URI: https://mywebsite.com/
-* Description: A plugin for Content Recommendation
-* Version: 1.0.0
-* Author: H.A.B.M. Faisal Akandha
-* Author URI: https://faisal.github.io/
-* License: GPL2
-**/
+ * Plugin Name: Recommendly
+ * Plugin URI: https://mywebsite.com/
+ * Description: A plugin for Content Recommendation
+ * Version: 1.0.0
+ * Author: H.A.B.M. Faisal Akandha
+ * Author URI: https://faisal.github.io/
+ * License: GPL2
+ **/
+
+function my_plugin_activate()
+{
+
+    add_option('Activated_Plugin', 'Plugin-Slug');
+
+    require_once(plugin_dir_path(__FILE__) . 'includes/database-manager.php');
+    create_recommendly_database();
+    add_option('cron_links', 0);
+}
+register_activation_hook(__FILE__, 'my_plugin_activate');
+
+function load_plugin()
+{
+
+    if (is_admin() && get_option('Activated_Plugin') == 'Plugin-Slug') {
+
+        delete_option('Activated_Plugin');
+    }
+}
+add_action('admin_init', 'load_plugin');
 
 require_once(plugin_dir_path(__FILE__) . 'admin/admin-menu.php');
-require_once(plugin_dir_path(__FILE__) . 'init.php');
 require_once(plugin_dir_path(__FILE__) . 'includes/cron-script.php');
+require_once(plugin_dir_path(__FILE__) . 'includes/create-simposts.php');
 require_once(plugin_dir_path(__FILE__) . 'frontend/posts.php');
 require_once(plugin_dir_path(__FILE__) . 'deactivation.php');
-
-?>
