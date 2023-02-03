bc16adb56e3442a6e2f98ec7583527e3f6854661
Faisal Akandha <57830958+faisalakandha@users.noreply.github.com>
2023-02-03 20:21:30 +0600

    Update logging.php

--- includes/logging.php
+++ includes/logging.php
@@ -1,21 +1,22 @@
 <?php
 
-if ( ! function_exists( 'plugin_log' ) ) {
-  function plugin_log( $entry, $mode = 'a', $file = 'WPRecommendly' ) { 
-    // Get WordPress uploads directory.
-    $upload_dir = wp_upload_dir();
-    $upload_dir = $upload_dir['basedir'];
-    // If the entry is array, json_encode.
-    if ( is_array( $entry ) ) { 
-      $entry = json_encode( $entry ); 
-    } 
-    // Write the log file.
-    $file  = $upload_dir . '/' . $file . '.log';
-    $file  = fopen( $file, $mode );
-    $bytes = fwrite( $file, current_time( 'mysql' ) . "::" . $entry . "\n" ); 
-    fclose( $file ); 
-    return $bytes;
+if (!function_exists('plugin_log')) {
+  function plugin_log($entry, $mode = 'a', $file = 'WPRecommendly')
+  {
+    if (get_option('recommendly_logs') != 0) {
+      // Get WordPress uploads directory.
+      $upload_dir = wp_upload_dir();
+      $upload_dir = $upload_dir['basedir'];
+      // If the entry is array, json_encode.
+      if (is_array($entry)) {
+        $entry = json_encode($entry);
+      }
+      // Write the log file.
+      $file  = $upload_dir . '/' . $file . '.log';
+      $file  = fopen($file, $mode);
+      $bytes = fwrite($file, current_time('mysql') . "::" . $entry . "\n");
+      fclose($file);
+      return $bytes;
+    }
   }
 }
-
-?>
